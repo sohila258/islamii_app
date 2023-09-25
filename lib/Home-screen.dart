58e6dce9f0e4f064f11294921efc94db79cd4408
sohila hitham <sohila01287179883@gmail.com>
2023-09-25 17:08:25 +0300

    initial project empty home screen

---
+++ lib/Home-screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routename = 'home-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islamii/Home-screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routename,
+      routes: {
+        HomeScreen.routename: (context) => HomeScreen(),
+      },
+    );
+  }
+}
